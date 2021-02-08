76b2164f23931361bbabe5cb67cba9e1f89d3ff1
DemoJameson <demojameson@gmail.com>
2021-02-08 23:02:11 +0800

    调整项目结构

--- GhostMod/GhostExtensions.cs
+++ GhostMod/GhostExtensions.cs
@@ -1,22 +1,10 @@
-﻿using Celeste.Mod;
-using Microsoft.Xna.Framework;
-using Monocle;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace Celeste.Mod.Ghost {
-    public static class GhostExtensions {
+public static class GhostExtensions {
+    private readonly static FieldInfo f_Player_wasDashB = typeof(Player).GetField("wasDashB", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        private readonly static FieldInfo f_Player_wasDashB = typeof(Player).GetField("wasDashB", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        public static bool GetWasDashB(this Player self)
-            => (bool) f_Player_wasDashB.GetValue(self);
-
-    }
+    public static bool GetWasDashB(this Player self)
+        => (bool) f_Player_wasDashB.GetValue(self);
+}
 }
